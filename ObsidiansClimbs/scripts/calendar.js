53275ec703b9aa64795d822d94c1c56198b71682
theoglick <theoglick@gmail.com>
2015-02-08 16:09:09 -0800

    Initial Commit

---
+++ ObsidiansClimbs/scripts/calendar.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function() {
+    $('#calendar').fullCalendar({
+        googleCalendarApiKey: 'AIzaSyCu3Fm5WmXxzrH2plG9eqRgSBZmXdG-8b0',
+        events: {
+            googleCalendarId: 'p7nntdaqcf4hu7ea2gadboagh4@group.calendar.google.com',
+            className: 'gcal-event' // an option!
+        }
+    });
+});
---
+++ ObsidiansClimbs/scripts/obsclimbs.js
@@ -0,0 +1,13 @@
+﻿
+var scrollDown = function (element, speed) {
+    var scrollto = $(element);
+        $('html, body').animate({
+        scrollTop: $(element).offset().top
+    }, speed);
+};
+
+var scrolltoTop = function (speed) {
+    $('html, body').animate({
+        scrollTop: '0'
+    }, speed);
+};
